3d27f3a2b292a300dbd4416ad69ddf6784171de2
ZeusTrmp <47661462+ZeusTrmp@users.noreply.github.com>
2019-02-18 09:18:17 -0300

    Add files via upload

---
+++ ExercicioCpf.py
@@ -0,0 +1,16 @@
+Nome= "Antonio"
+Tell= 123456789
+Cpf= 1110987654321
+Altura= 1.76
+
+print ("O seu nome é:",Nome)
+print ("O seu Telefone é:", Tell)
+print ("O seu Cpf é:",Cpf)
+print ("A sua altura é",Altura)
+
+print (type(Nome))
+print (type(Tell))
+print (type(Cpf))
+print (type(Altura))
+
+
